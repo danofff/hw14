080c9c64885bb58ab00564a4f793d8b71a4229ad
dan <danofff@mail.ru>
2017-12-28 15:41:34 +0600

    Добавьте файлы проекта.

---
+++ hw14/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace hw14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            task6();
+        }
+
+        static void task6()
+        {
+            XmlDocument doc = new XmlDocument();
+            XmlDeclaration declaration=doc.CreateXmlDeclaration("1.0", "windows-1251", "yes");
+            doc.AppendChild(declaration);
+            XmlElement tovar = doc.CreateElement("tovar");           
+            XmlElement name = doc.CreateElement("name");
+            name.InnerText = "Бензин";          
+            XmlElement price = doc.CreateElement("price");
+            price.InnerText = "20";
+            tovar.AppendChild(name);
+            tovar.AppendChild(price);
+            doc.AppendChild(tovar);
+            doc.Save("1.xml");
+        }
+
+    }
+}
